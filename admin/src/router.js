a823ba76c3b2b141fb1491a15aa25d17c61af35c
刘溜遛 <756792312@qq.com>
2021-09-07 15:56:18 +0800

    feat：子页面

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,8 +1,8 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/login.vue"
-// import Admin from "./views/admin.vue"
-// import Welcome from "./views/admin/welcome.vue"
+import Admin from "./views/admin.vue"
+import Welcome from "./views/admin/welcome.vue"
 // import Category from "./views/admin/category.vue"
 // import Course from "./views/admin/course.vue"
 // import Chapter from "./views/admin/chapter.vue"
@@ -25,72 +25,76 @@ export default new Router({
             path: "*",
             redirect: "/login",
         }, {
-            path: "",
-            redirect: "/login",
-        }, {
             path: "/login",
             component: Login
-        }],
-    // }, {
-    //     path: "/",
-    //     name: "admin",
-    //     component: Admin,
-    //     meta: {
-    //         loginRequire: true
-    //     },
-    //     children: [{
-    //         path: "welcome",
-    //         name: "welcome",
-    //         component: Welcome,
-    //     }, {
-    //         path: "business/category",
-    //         name: "business/category",
-    //         component: Category,
-    //     }, {
-    //         path: "business/course",
-    //         name: "business/course",
-    //         component: Course,
-    //     }, {
-    //         path: "business/chapter",
-    //         name: "business/chapter",
-    //         component: Chapter,
-    //     }, {
-    //         path: "business/section",
-    //         name: "business/section",
-    //         component: Section,
-    //     }, {
-    //         path: "business/content",
-    //         name: "business/content",
-    //         component: Content,
-    //     }, {
-    //         path: "business/teacher",
-    //         name: "business/teacher",
-    //         component: Teacher,
-    //     }, {
-    //         path: "business/member",
-    //         name: "business/member",
-    //         component: Member,
-    //     }, {
-    //         path: "business/sms",
-    //         name: "business/sms",
-    //         component: Sms,
-    //     }, {
-    //         path: "file/file",
-    //         name: "file/file",
-    //         component: File,
-    //     }, {
-    //         path: "system/user",
-    //         name: "system/user",
-    //         component: User,
-    //     }, {
-    //         path: "system/resource",
-    //         name: "system/resource",
-    //         component: Resource,
-    //     }, {
-    //         path: "system/role",
-    //         name: "system/role",
-    //         component: Role,
-    //     }]
-    // }]
-}
+        }, {
+            path: "/admin",
+            component: Admin,
+            children: [{
+                path: "welcome",
+                component: Welcome
+            }]
+        }]
+//  {
+//     path: "/",
+//     name: "admin",
+//     component: Admin,
+//     meta: {
+//         loginRequire: true
+//     },
+//     children: [{
+//         path: "welcome",
+//         name: "welcome",
+//         component: Welcome,
+//     }, {
+//         path: "business/category",
+//         name: "business/category",
+//         component: Category,
+//     }, {
+//         path: "business/course",
+//         name: "business/course",
+//         component: Course,
+//     }, {
+//         path: "business/chapter",
+//         name: "business/chapter",
+//         component: Chapter,
+//     }, {
+//         path: "business/section",
+//         name: "business/section",
+//         component: Section,
+//     }, {
+//         path: "business/content",
+//         name: "business/content",
+//         component: Content,
+//     }, {
+//         path: "business/teacher",
+//         name: "business/teacher",
+//         component: Teacher,
+//     }, {
+//         path: "business/member",
+//         name: "business/member",
+//         component: Member,
+//     }, {
+//         path: "business/sms",
+//         name: "business/sms",
+//         component: Sms,
+//     }, {
+//         path: "file/file",
+//         name: "file/file",
+//         component: File,
+//     }, {
+//         path: "system/user",
+//         name: "system/user",
+//         component: User,
+//     }, {
+//         path: "system/resource",
+//         name: "system/resource",
+//         component: Resource,
+//     }, {
+//         path: "system/role",
+//         name: "system/role",
+//         component: Role,
+//     }]
+// }]
+    }
 )
---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>您好！欢迎进入子页面</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
